cba2f5d9d9717ffef2e8b67fce89cf0b92a3c466
Ryan-Chiang <ryanjiang@outlook.com>
2019-04-25 15:32:09 +0800

    512ms faster than 12.52%,29.3MB less than 5.12%

---
+++ 3_longest_substring/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3_longest_substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = new Solution().LengthOfLongestSubstring("alkjd");
+            Console.WriteLine(res);
+        }
+    }
+
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int i = 0, l = s.Length, j = 0;
+            Dictionary<char[], int> dictContainer = new Dictionary<char[], int>();
+
+            List<char> lstCurrent = new List<char>(l);
+
+            char tmp = char.MinValue;
+
+            for (; i < l; i++)
+            {
+                tmp = s[i];
+                if (lstCurrent.Contains(tmp))
+                {
+                    dictContainer.Add(lstCurrent.ToArray(), lstCurrent.Count());
+                    lstCurrent.Clear();
+                    i = j++;
+                }
+                else
+                {
+                    lstCurrent.Add(tmp);
+                }
+            }
+            dictContainer.Add(lstCurrent.ToArray(), lstCurrent.Count());
+
+            return dictContainer.Values.Max();
+        }
+    }
+}
